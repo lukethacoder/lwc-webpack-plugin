6868c62a91d1fecdd732fac765b47fba4478e8eb
Luke Secomb <luke.secomb@live.com>
2023-10-23 12:54:11 +1100

    📦 bump packages / add CSS only module to playground test

--- playground/webpack.config.js
+++ playground/webpack.config.js
@@ -1,8 +1,16 @@
+const path = require('path')
 const LwcWebpackPlugin = require('@lukethacoder/lwc-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
   plugins: [
     new LwcWebpackPlugin({
       rootDir: './',
